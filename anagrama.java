604c3c4c013bd86d70500f765c3fc5cbc6805f59
Andres <afguevarac@ufpso.edu.co>
2024-10-04 17:49:13 -0500

    price

---
+++ anagrama.java
@@ -0,0 +1,24 @@
+public class anagrama {
+    public static void main(String[] args) {
+        char[] palabra1 ={'A','M','O','R'};
+        char[] palabra2 ={'M','O','R','A'};
+        int contador=0;
+        int long_palabra1=palabra1.length;
+        int long_palabra2=palabra2.length;
+        for (int i=0;i<long_palabra1;i++){
+            for (int j=0 ;j<long_palabra2;j++){
+                if (palabra1[i]==palabra2[j]){
+                    contador++;
+                    palabra2[j]=' ';
+                    break;
+                }
+            }
+            
+        }
+        if (contador==long_palabra1){
+            System.out.println("son anagramas");
+        }else{
+            System.out.println("no son ");
+        }
+    }
+}
